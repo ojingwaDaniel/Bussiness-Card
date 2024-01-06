029dd22ce13678081f01bfcb9ad4773c95ad2047
ojingwaDaniel <ojingwadanny@gmail.com>
2024-01-06 15:14:43 +0100

    added a responsive media for small devices

--- src/component/footer.jsx
+++ src/component/footer.jsx
@@ -1,12 +1,15 @@
 import FacebookICon from './images/Facebook Icon.png'
 import InstagramIcon from './images/Instagram Icon.png'
+import GithubICon from './images/GitHub Icon.png'
+import TwitterIcon from './images/Twitter Icon.png'
 
 export default function Footer() {
     return (
       <div className="footer">
-        <img src="" alt="" />
-        <img src="" alt="" />
-        <img src="" alt="" />
+        <img src={FacebookICon} alt="NoImage" />
+        <img src={InstagramIcon} alt="NoImage" />
+        <img src={GithubICon} alt="NoImage" />
+        <img src={TwitterIcon} alt="NoImage" />
       </div>
     );
 }
